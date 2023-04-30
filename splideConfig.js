f4170263d83345f6fffee30f29698a902bc051c6
jhulian11 <jhuliangamer@gmail.com>
2023-04-30 19:40:29 -0300

    Section Populares-parceiros corrigida e parceiros adicionado (falta ajustar responsivo)

--- splideConfig.js
+++ splideConfig.js
@@ -1,5 +1,5 @@
 const splideConfig = function () {
-  const splideOptions = {
+  const splideEquipeOptions = {
     start: 1,
     perPage: 1,
     focus: "center",
@@ -14,16 +14,29 @@ const splideConfig = function () {
         type: "loop",
         perPage: 1,
         arrows: true,
-        destroy:false
+        destroy: false,
       },
       4000: {
         destroy: true,
       },
     },
   };
+  const splideParceirosOptions = {
+    start: 0,
+    perPage: 1,
+    focus: "start",
+    trimSpace: false,
+    arrows: false,
+    pagination: false,
+    autoWidth: true,
+    gap: "3rem",
+  
+  };
 
-  const splide = new Splide(`.splide`, splideOptions);
-  splide.mount();
+  const splideEquipe = new Splide(`#splide-equipe`, splideEquipeOptions);
+  const splideParceiros = new Splide(`#splide-parceiros`, splideParceirosOptions);
+  splideEquipe.mount();
+  splideParceiros.mount();
 };
 
 splideConfig();
