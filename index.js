fb87f6ab3a2afb54168a2513dd4104698473dcee
jhulian11 <jhuliangamer@gmail.com>
2023-05-01 19:24:53 -0300

    Teste Enviando email (ainda nao ta pronto)

--- index.js
+++ index.js
@@ -23,10 +23,51 @@ const paginaApp = {
       }
     });
   },
+  formSubmit(form) {
+    const enviarEmail = (data) => {
+      fetch("https://formsubmit.co/ajax/jhuliangamer@gmail.com", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          console.log(response);
+          if (response.ok) {
+            // operação concluída com sucesso
+            console.log("Operação concluída com sucesso!");
+          } else {
+            // operação falhou
+            console.log("Operação falhou!");
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
+    form.addEventListener("submit", () => {
+      const nome = form.querySelector("#input-nome").value;
+      const email = form.querySelector("#input-email").value;
+      const mensagem = form.querySelector("#input-mensagem").value;
+
+      const data = {
+        name: nome,
+        email: email,
+        message: mensagem,
+      };
+      console.log(data);
+
+      enviarEmail(data);
+    });
+  },
   init() {
     splideConfig();
     justValidateConfig();
     this.sectionParceiro();
+
+    // this.formSubmit(document.querySelector("#form-email"));
   },
 };
 
