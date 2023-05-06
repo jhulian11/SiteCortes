a60996d84a23346d08907eae358b27f69f075689
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2023-05-06 18:06:11 -0300

    last video 2.0

--- script/lastEpYoutube.js
+++ script/lastEpYoutube.js
@@ -2,10 +2,10 @@
 const API_KEY = "sua_chave_de_api_aqui";
 
 // Defina o ID do canal do YouTube
-const CHANNEL_ID = "id_do_canal_aqui";
+const CHANNEL_ID = "@cortescast5910";
 
 // Defina o número máximo de resultados que você deseja receber
-const MAX_RESULTS = 10;
+const MAX_RESULTS = 4;
 
 // Faça a solicitação para a API do YouTube Data
 axios
@@ -18,6 +18,7 @@ axios
     videos.forEach((video) => {
       const title = video.snippet.title;
       const videoId = video.id.videoId;
+      console.log(video);
       console.log(`Título: ${title}, ID do vídeo: ${videoId}`);
     });
   })
